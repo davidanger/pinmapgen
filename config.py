da18f76d02e508be76ebe9a31561316f0fb0bea7
davidanger <davidanger@163.com>
2018-08-02 18:07:17 +0800

    Fist Creat

---
+++ config.py
@@ -0,0 +1,6 @@
+class PINFORMAT_CONFIG:
+    AF_NUM = 15
+    PIN_NUM = 15
+    PIN_GRP = ['PA', 'PB', 'PC', 'PD', 'PE', 'PF', 'PG', 'PH', 'PI']
+    PIN_CAT = ['I2C', 'UART', 'USART', 'TIM', 'LPTIM', 'DFSDM', 'CEC', 'SPI', 'I2S', 'SAI', 'SPDIF', 'OTG',
+               'QUADSPI', 'FMC', 'LCD', 'CAN', 'SDMMC', 'ETH', 'MDIOS', 'DCMI', 'DSI']
