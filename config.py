0727f363fe44b30082b9da4a862eaee778e52e4a
davidanger <davidanger@163.com>
2018-08-03 18:03:21 +0800

    Second Working

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+class DATA:
+    INPUT_FILE = 'input.txt'
+    OUTPUT_FILE = 'output.txt'
+
 class PINFORMAT_CONFIG:
     AF_NUM = 15
     PIN_NUM = 15
+    PIN_GLEN = 2
     PIN_GRP = ['PA', 'PB', 'PC', 'PD', 'PE', 'PF', 'PG', 'PH', 'PI']
     PIN_CAT = ['I2C', 'UART', 'USART', 'TIM', 'LPTIM', 'DFSDM', 'CEC', 'SPI', 'I2S', 'SAI', 'SPDIF', 'OTG',
-               'QUADSPI', 'FMC', 'LCD', 'CAN', 'SDMMC', 'ETH', 'MDIOS', 'DCMI', 'DSI']
+               'QUADSP', 'FMC', 'LCD', 'CAN', 'SDMMC', 'ETH', 'MDIOS', 'DCMI', 'DSI', 'EVENTOUT']
--- run.py
+++ run.py
@@ -1,1 +1,34 @@
 import config
+
+# open input file
+f = open(config.DATA.INPUT_FILE, 'r')
+datain = f.read()
+
+# Remove '\n'
+while datain.find('\n') > 0:
+    i = datain.find('\n')
+    for pincat in config.PINFORMAT_CONFIG.PIN_CAT:
+        t = len(pincat)
+        tc = datain[i+1:i+1+t]
+        tcpg = datain[i+1:i+1+config.PINFORMAT_CONFIG.PIN_GLEN]
+        print(pincat, t)
+        print(tc)
+        if datain[i-1] == '/':
+            # means AA/\nBB
+            datain = datain[:i] + datain[i + 1:]
+            break
+        if pincat == tc \
+                or tcpg in config.PINFORMAT_CONFIG.PIN_GRP \
+                or datain[i+1] == '-':
+            #
+            # means PXx
+            # means XX\n-
+            datain = datain.replace('\n', ' ', 1)
+            break
+    if datain[i] == '\n':
+        datain = datain[:i] + datain[i+1:]
+
+# convert input to a list
+datalist = datain.split()
+
+# make a PIN definition class
