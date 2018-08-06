d7824affbb58badc95c14a8e9502c455e307fbe4
davidanger <davidanger@163.com>
2018-08-06 16:27:10 +0800

    Release Version 0.1

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 class DATA:
     INPUT_FILE = 'input.txt'
-    OUTPUT_FILE = 'output.txt'
+    OUTPUT_FILE = 'output.csv'
 
 class PINFORMAT_CONFIG:
     AF_NUM = 15
     PIN_NUM = 15
     PIN_GLEN = 2
-    PIN_GRP = ['PA', 'PB', 'PC', 'PD', 'PE', 'PF', 'PG', 'PH', 'PI']
+    PIN_GRP = ['PA', 'PB', 'PC', 'PD', 'PE', 'PF', 'PG', 'PH', 'PI', 'PJ', 'PK']
     PIN_CAT = ['I2C', 'UART', 'USART', 'TIM', 'LPTIM', 'DFSDM', 'CEC', 'SPI', 'I2S', 'SAI', 'SPDIF', 'OTG',
-               'QUADSP', 'FMC', 'LCD', 'CAN', 'SDMMC', 'ETH', 'MDIOS', 'DCMI', 'DSI', 'EVENTOUT']
+               'QUADSP', 'FMC', 'LCD', 'CAN', 'SDMMC', 'ETH', 'MDIOS', 'DCMI', 'DSI', 'EVEN','JTD']
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import config
 
 # open input file
-f = open(config.DATA.INPUT_FILE, 'r')
-datain = f.read()
+fin = open(config.DATA.INPUT_FILE, 'r')
+datain = fin.read()
+fin.close()
 
 # Remove '\n'
 while datain.find('\n') > 0:
@@ -11,8 +12,8 @@ while datain.find('\n') > 0:
         t = len(pincat)
         tc = datain[i+1:i+1+t]
         tcpg = datain[i+1:i+1+config.PINFORMAT_CONFIG.PIN_GLEN]
-        print(pincat, t)
-        print(tc)
+        # print(pincat, t)
+        # print(tc)
         if datain[i-1] == '/':
             # means AA/\nBB
             datain = datain[:i] + datain[i + 1:]
@@ -24,11 +25,29 @@ while datain.find('\n') > 0:
             # means PXx
             # means XX\n-
             datain = datain.replace('\n', ' ', 1)
+            print(tc)
             break
     if datain[i] == '\n':
-        datain = datain[:i] + datain[i+1:]
+        if i >= len(datain) - 1:
+            datain = datain[:i]
+        else:
+            datain = datain[:i] + datain[i+1:]
 
 # convert input to a list
 datalist = datain.split()
 
-# make a PIN definition class
+# Output CSV format
+fout = open(config.DATA.OUTPUT_FILE, 'w')
+fout.write('PIN')
+for i in range(0, (config.PINFORMAT_CONFIG.AF_NUM + 1)):
+    fout.write(',AF{:d}'.format(i))
+fout.write('\n')
+
+i = 0
+while i * (config.PINFORMAT_CONFIG.AF_NUM + 2) < (len(datalist) - (config.PINFORMAT_CONFIG.AF_NUM + 1)):
+    tempout = datalist[i * (config.PINFORMAT_CONFIG.AF_NUM + 2):(i+1) * (config.PINFORMAT_CONFIG.AF_NUM + 2)]
+    print(tempout)
+    fout.write(','.join(tempout) + '\n')
+    i = i + 1
+fout.close()
+
